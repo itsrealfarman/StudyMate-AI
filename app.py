bf47d658db90b7d1ca55f9cac0cb46954daee558
Farman Ali <fa0472439@gmail.com>
2026-07-22 19:11:45 +0500

    Add PDF extraction and text chunking

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from utils.pdf_reader import extract_text
+from utils.text_splitter import split_text
 import streamlit as st
 
 st.set_page_config(
@@ -12,4 +14,15 @@ st.write("Your AI-Powered Study Assistant")
 uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
 
 if uploaded_file:
-    st.success(f"Uploaded: {uploaded_file.name}")
+    text = extract_text(uploaded_file)
+
+    chunks = split_text(text)
+
+    st.success("PDF Loaded Successfully!")
+
+    st.subheader("Extracted Text")
+    st.write(text[:2000])
+
+    st.subheader("Chunks")
+    st.write(f"Total Chunks: {len(chunks)}")
+    st.write(chunks[0])
