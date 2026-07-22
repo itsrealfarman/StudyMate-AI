95131733581afea2001b7087bfc42811d6c94201
Farman Ali <fa0472439@gmail.com>
2026-07-22 19:30:28 +0500

    Add gitignore and project structure

---
+++ utils/vector_store.py
@@ -0,0 +1,18 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_vector_store(chunks):
+    embeddings = OpenAIEmbeddings(
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+
+    vector_store = FAISS.from_texts(
+        texts=chunks,
+        embedding=embeddings
+    )
+
+    return vector_store
